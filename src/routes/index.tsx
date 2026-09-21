12d1c100e07e61835cfd74267764e75c48fabf46
santanaalloah-del <santanaalloah@gmail.com>
2026-09-21 15:29:11 -0300

    Add kitchen image import to index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -30,6 +30,7 @@ import dominic from "@/assets/dominic-candid.jpg";
 import room from "@/assets/dominic-room.jpg";
 import livingRoom from "@/assets/living-room.png";
 import bedroom from "@/assets/bedroom.png";
+import kitchen from "@/assets/kitchen.png";
 import floorPlan from "@/assets/apartment-floor-plan.png";
 import cafe from "@/assets/cafe-hands.jpg";
 import street from "@/assets/rain-street.jpg";
