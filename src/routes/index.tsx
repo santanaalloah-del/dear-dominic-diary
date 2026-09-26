3eb005b7b6d026c9bdf0cc2ef9c5ff6589809b7b
santanaalloah-del <santanaalloah@gmail.com>
2026-09-26 11:01:09 -0300

    Update button click handler for planning date

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6261,16 +6261,15 @@ const markAsLived = async (
         </button>
       </div>
 
-      {!addingDate && (
+{!planningDate && (
   <button
     type="button"
     className="dates-sticky-add"
-    onClick={() => setAddingDate(true)}
+    onClick={() => setPlanningDate(true)}
   >
     ＋ Plan another date
   </button>
 )}
-
       {planningDate ? (
         <section className="dates-empty">
           <small>
